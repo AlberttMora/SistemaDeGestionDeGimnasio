a0e2d43d94aaa772c37695314bd3849e9dbe4b0e
Melany <judithsirias2006@gmail.com>
2026-06-21 10:38:13 -0600

    View terminado

--- src/com/gym/init/Main.java
+++ src/com/gym/init/Main.java
@@ -1,8 +1,10 @@
 package com.gym.init;
 
+import com.gym.mvc.view.VentanaPrincipal;
+import javax.swing.SwingUtilities;
+
 public class Main {
-	
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new VentanaPrincipal().setVisible(true));
+    }
 }
