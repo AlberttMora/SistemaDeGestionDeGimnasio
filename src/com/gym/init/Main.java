42f64a77f5811c41de45224fd14d04877ee74932
AlberttMora <moraAlbertt13092022@gmail.com>
2026-06-26 00:24:25 -0600

    xd

--- src/com/gym/init/Main.java
+++ src/com/gym/init/Main.java
@@ -1,20 +1,10 @@
 package com.gym.init;
  
 import com.gym.mvc.controllers.loggin.ControllerLoggin;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
  
 public class Main {
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(() -> {
-			try {
-				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
- 
 			new ControllerLoggin().init();
-		});
 	}
 }
  
